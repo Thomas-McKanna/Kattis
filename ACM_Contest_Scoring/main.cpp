ffc11f522718abfbbc942eedd1d3817b00f20617
Thomas McKanna <thomasmckanna@gmail.com>
2018-09-16 16:06:40 -0500

    ACM Contest Scoring - Accepted.

--- ACM_Contest_Scoring/main.cpp
+++ ACM_Contest_Scoring/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+using std::cin;
+using std::cout;
+using std::string;
+
+// Primary Measure = number of problems solved
+// Secondary Measure = sum of rimes for right answers + 20*number of wrong submissions
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int minutes;
+    char problem;
+    string result;
+
+    int score_solved = 0;
+    int score_time = 0;
+
+    int wrong_count[26] = {0};
+
+    cin >> minutes;
+    while (minutes != -1) {
+        cin >> problem;
+        cin >> result;
+
+        if (result == "wrong") {
+            wrong_count[problem - 'A']++;
+        }
+
+        if (result == "right") {
+            score_solved++;
+            score_time += minutes + 20*wrong_count[problem - 'A'];
+        }
+
+        cin >> minutes;
+    }
+    cout << score_solved << " " << score_time;
     return 0;
 }
