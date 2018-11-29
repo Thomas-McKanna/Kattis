64229567294ec073b8cf02439fdab4410bd6f532
Thomas McKanna <thomasmckanna@gmail.com>
2018-11-29 12:15:49 -0600

    Finished Euler's number. It was only precise on a linux machine (which supported 8 byte longs).

--- Eulers_Number/main.cpp
+++ Eulers_Number/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
-char ctoi[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
-long long factorial(int n) {
+unsigned long factorial(int n) {
     if (n <= 0) {
         return 1;
     }
 
-    int sum = 1;
+    unsigned long sum = 1;
     for (; n > 0; n--) {
         sum *= n;
     }
@@ -19,39 +19,16 @@ long long factorial(int n) {
     return sum;
 }
 
-string div(int n) {
-    string num;
-    if (n == 1) {
-        return string("1.000000000000000");
-    } else {
-        int curr = 10;
-        num.push_back('0');
-        num.push_back('.');
-        for (int i = 0; i < 15; i++) {
-            if (curr / n != 0) {
-                num.push_back(ctoi[curr / n]);
-                curr = ((curr - ((curr / n) * n)) * 10);
-            } else {
-                num.push_back('0');
-                curr *= 10;
-            }
-        }
-        return num;
-    }
-}
-
 int main()
 {
     int n;
     cin >> n;
 
-    vector<string> nums;
-    for (int i = 0; i <= n; i++) {
-        nums.push_back(div(factorial(i)));
-        cout << div(factorial(i)) << endl;
-        cout << fixed << setprecision(15) << 1.0 / factorial(i) << endl;
-        cout << "Factorial of " << i << ": " << factorial(i) << endl;
+    double sum = 0;
+    for (int i = 0; i <= n && i <= 16; i++) {
+        sum += 1.0 / factorial(i);
     }
 
+    cout << fixed << setprecision(15) << sum << endl;
     return 0;
 }
