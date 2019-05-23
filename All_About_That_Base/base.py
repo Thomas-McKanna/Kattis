e17c2aa5a3259fae4d3208fcbcc137f575b5105a
Thomas McKanna <thomasmckanna@gmail.com>
2019-05-22 20:16:23 -0500

    Did a lot of work on All About That Base.

---
+++ All_About_That_Base/base.py
@@ -0,0 +1,78 @@
+def symbol_to_base(symbol):
+    val = None
+    if symbol.isdigit():
+        val = int(symbol)
+    elif symbol.isalpha():
+        # 'a' is 10 and so on
+        val = ord(symbol) - ord('a') + 10
+    return val
+
+def base_to_symbol(base):
+    if 1 <= base and base >= 9:
+        return str(base)
+    else:
+        return chr(ord('a') + base - 10)
+
+def to_decimal(number, base):
+    """ 
+    Returns the number in decimal, or None 
+    if no conversion possible. 
+
+    number should be a String
+    base should be 1-36 (int)
+    """
+    s = 0
+    position = 0
+    for n in reversed(number):
+        n = symbol_to_base(n)
+        if n >= base:
+            return None
+        s += n**position
+        position += 1
+    return s
+
+def add(first, second):
+    return first + second
+
+def sub(first, second):
+    return first - second
+
+def mul(first, second):
+    return first * second
+
+def div(first, second):
+    return first // second
+
+op_to_func = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': div
+}
+
+n = int(input())
+
+for _ in range(n):
+    fields = input().split()
+    operand1 = fields[0]
+    op = fields[1]
+    operand2 = fields[2]
+    result = fields[4]
+
+    found_valid_base = False
+    for i in range(1, 37):
+        op1 = to_decimal(operand1, i)
+        op2 = to_decimal(operand2, i)
+        r = to_decimal(result, i)
+
+        if op1 == None or op2 == None or r == None:
+            continue
+
+        if op_to_func[op](op1, op2) == r:
+            found_valid_base = True
+            print(base_to_symbol(i), end='') 
+
+    if found_valid_base:
+        print()
+    else:
+        print('invalid')
