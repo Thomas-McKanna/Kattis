81356093b433ac4664a14687b7c224aa79cf6192
thomas <thomasmckanna@gmail.com>
2020-10-20 10:12:25 -0500

    Finished Watering Grass.

---
+++ Watering_Grass/water.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+
+using namespace std;
+
+typedef pair<double, double> dd;
+
+int main() {
+    int n, l, w, pos, rad;
+    dd intervals[10000];
+    double half_rec_w;
+    while (scanf("%d %d %d", &n, &l, &w) != EOF) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d %d", &pos, &rad);
+            if (w / 2.0 > rad) {
+                intervals[i] = dd(999999, 999999 + 1);
+            } else {
+                half_rec_w = sqrt(pow(rad, 2) - pow(w, 2) / 4.0);
+                intervals[i] = dd(pos - half_rec_w, pos + half_rec_w);
+            }
+        }
+
+        sort(intervals, intervals + n);
+
+        double covered = 0.0;
+        int best_choice = 0, current = 0, ans = 0;
+        while (covered < l && current < n) {
+            while (intervals[current].first <= covered) {
+                if (intervals[current].second > intervals[best_choice].second) {
+                    best_choice = current;
+                }
+                current++;
+            }
+            if (intervals[best_choice].first > covered) {
+                ans = -1;
+                break;
+            }
+            covered = intervals[best_choice].second;
+            ans++;
+            best_choice = current;
+        }
+
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
